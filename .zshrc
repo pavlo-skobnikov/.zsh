361c33020c92664203dd6d0a7d61bf7bb0db35a4
Pavlo Skobnikov <pavlo.skobnikov@gmail.com>
2024-05-09 17:37:23 +0300

    Optimise `compinit`

--- .zshrc
+++ .zshrc
@@ -1,6 +1,12 @@
 #!/bin/sh
 
+# `zsh/zprof` is a module that provides a way to profile zsh startup time.
+# zmodload zsh/zprof
+
 # Source zsh configuration files
 while IFS= read -r -d '' file; do
   source "$file"
 done < <(find "$HOME/.config/zsh/sources" -type f -name "*.zsh" -print0)
+
+# Uncommenting the line below is a must when profiling.
+# zprof
--- sources/homebrew_completions.zsh
+++ sources/homebrew_completions.zsh
@@ -1,8 +1,14 @@
-# Load Homebrew completions into the shell
+# Load Homebrew completions into the shell.
 if type brew &>/dev/null
 then
   FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
 
   autoload -Uz compinit
-  compinit
+
+  # Run `compini` only once per day.
+  for dump in ~/.zcompdump(N.mh+24); do
+    compinit
+  done
+
+  compinit -C
 fi
